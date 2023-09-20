9de3260eac2f035824b97bca8b2d3897e793e11f
Thomas Krut <thomas.k.krut@gmail.com>
2023-09-20 21:44:53 +0200

    new structure

--- dir.go
+++ dir.go
@@ -1,13 +1,46 @@
 package main
 
-var (
-	None      = direction{varX: 0, varY: 0}
-	North     = direction{varX: 0, varY: -1}
-	South     = direction{varX: 0, varY: 1}
-	East      = direction{varX: 1, varY: 0}
-	West      = direction{varX: -1, varY: 0}
+import (
+	"strconv"
 )
 
-type direction struct {
+
+
+var (
+	None      = Direction{varX: 0, varY: 0}
+	North     = Direction{varX: 0, varY: -1}
+	South     = Direction{varX: 0, varY: 1}
+	East      = Direction{varX: 1, varY: 0}
+	West      = Direction{varX: -1, varY: 0}
+)
+
+type Direction struct {
 	varX, varY int
+}
+
+type DirectionQueue []Direction
+
+func (q DirectionQueue) String() string {
+	str := ""
+	for _, dir := range q {
+		str += "(x:" + strconv.Itoa(dir.varX) + " y:" + strconv.Itoa(dir.varY) + ")" 
+	}
+	return str
+}
+
+func (q *DirectionQueue) pop() Direction {
+	if len(*q) == 0 {
+		return None
+	}
+	dir := (*q)[0]
+	*q = (*q)[1:]
+	return dir
+}
+
+func (q *DirectionQueue) push(dir Direction) {
+	if (len(*q) > 0 && (*q)[len(*q)-1] != dir) || len(*q) == 0 {
+		
+		*q = append(*q, dir)
+
+}
 }
--- main.go
+++ main.go
@@ -1,80 +1,82 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
 	"image/color"
 	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	_"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type BodyElement struct {
-	x, y int
-	dir  direction
-	rect *ebiten.Image
-	ops  *ebiten.DrawImageOptions
-	tail *BodyElement
-}
-
-func (e *BodyElement) getEndOfTail() *BodyElement {
-	if e.tail == nil {
-		return e
-	}
-	return e.tail.getEndOfTail()
-}
-
-func (e *BodyElement) move() {
-	
-}
-
-func newBodyElement(startX, startY int) *BodyElement {
-	newElement := &BodyElement{x: startX, y: startY, dir: East, ops: &ebiten.DrawImageOptions{}, rect: ebiten.NewImage(8, 8)}
-	newElement.rect.Fill(color.RGBA{255, 0, 255, 255})
-	return newElement
-}
-
 var (
-	head *BodyElement
+	snake *Snake
 	grow int
+	tile *ebiten.Image
+	img *ebiten.Image
+	ops = &ebiten.DrawImageOptions{}
+	dirQueue DirectionQueue
+)
+
+const (
+	elementSize = 10
 )
 
 func init() {
-	head = newBodyElement(10, 10)
-	
+	snake = newSnake()
+	img = ebiten.NewImage(elementSize, elementSize)
+	img.Fill(color.White)
+	tile = ebiten.NewImage(elementSize - 1, elementSize -1)
+	tile.Fill(color.RGBA{120,120,120,120})
 }
 
-func getNewDirection(currentDir direction) direction {
+func keys() (Direction, bool) {
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		return North
+		return North, true
 	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		return South
+		return South, true
 	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		return West
+		return West, true
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		return East
-	} else if ebiten.IsKeyPressed(ebiten.KeySpace) {
-		grow++
+		return East, true
 	}
-	return currentDir
+	return None, false
 }
 
 type Game struct {
 }
 
 func (g *Game) Update() error {
+
+	if newDir, ok := keys(); ok {
+		dirQueue.push(newDir)
+	}
+
 	if grow > 0 {
 		grow--
-		tail := head.getEndOfTail()
+		tail := snake.head.getEndOfTail()
 		tail.tail = newBodyElement(tail.x, tail.y)
 	}
-	head.dir = getNewDirection(head.dir)
-	head.ops.GeoM.Reset()
-	head.x += head.dir.varX
-	head.y += head.dir.varY
-	head.ops.GeoM.Translate(float64(head.x), float64(head.y))
+	
+	if len(dirQueue) > 0 && snake.head.x % elementSize == 0 && snake.head.y % elementSize == 0 {
+		snake.dir = dirQueue.pop()
+	}
+
+	snake.head.x += snake.dir.varX
+	snake.head.y += snake.dir.varY
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(head.rect, head.ops)
+	for x:=0; x<32; x++ {
+		for y:=0; y < 24; y++ {
+			ops.GeoM.Reset()
+			ops.GeoM.Translate(float64(x * elementSize), float64(y * elementSize))
+			screen.DrawImage(tile, ops)
+		}
+	}
+	ops.GeoM.Reset()
+	ops.GeoM.Translate(float64(snake.head.x), float64(snake.head.y))
+	screen.DrawImage(img, ops)	
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -83,7 +85,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Gosnake")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
